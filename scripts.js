6dcf64371e4cd1a21cf1d77cd95be328208bc479
Brittany Storoz <brittanystoroz@gmail.com>
2017-09-08 10:40:32 -0600

    Lorem ipsum dolor set amet

--- scripts.js
+++ scripts.js
@@ -2,4 +2,5 @@ console.log("Did it!");
 console.log("Another merge conflict!");
 console.log("Will this conflict?");
 console.log('THird time is the charm.');
+console.log("So conflicted!");
 console.log('Chika chika slim shady');
